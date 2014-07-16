f2b793d84fdb0c81027cd76804c7698b1860665c
Chris Harrington <chrisharrington99@gmail.com>
2014-07-15 23:47:34 -0600

    Moved slideshow html into template file.

--- public/scripts/directives/slideshow.js
+++ public/scripts/directives/slideshow.js
@@ -1,18 +1,18 @@
-Logger.app.directive("ngSlideshow", function () {
+Logger.app.directive("ngSlideshow", function ($compile) {
 	return {
 		restrict: "A",
-		compile: function(element, attributes) {
-			$(element).html("<div class=\"spinner\"><img src=\"[[url]]\" ng-repeat=\"url in " + attributes.ngSlideshow + "\" /></div>");
+		templateUrl: "templates/slideshow.html",
+		compile: function(scope, element, attributes) {
+			var pictures = scope.$eval(attributes.ngSlideshow);
+			if (!pictures || pictures.length == 0)
+				return;
+
+			scope.pictures = pictures;
+
+			//return $compile(element);
 		},
 		link: function(scope, element, attributes) {
-//			$(element).find("img").each(function() {
-//				var width = $(this).width();
-//				var height = $(this).height();
-//				var clipSize = 200;
-//				var x = ((width - clip/2) - clip/2);
-//				var y = ((height - clip/2) - clip/2);
-//				$(this).css("clip", "rect(" + y + "px, " + x + "px, " + x + "px, " + y + "px)");
-//			});
+			debugger;
 		}
 	};
 });
