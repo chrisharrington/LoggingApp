995e8979e2115435baaee5961a6e82c200994e53
Chris Harrington <chrisharrington99@gmail.com>
2014-07-17 12:24:18 -0600

    Modifications to modal show code.

--- public/scripts/directives/modal.js
+++ public/scripts/directives/modal.js
@@ -3,17 +3,15 @@ Logger.app.directive("modal", function() {
 		restrict: "E",
 		templateUrl: "templates/modal.html",
 		transclude: true,
-		link: {
-			pre: function(scope, element, attributes) {
-				scope.title = attributes.header;
-				scope.className = attributes.class;
-				scope.close = scope.$eval(attributes.close);
-				scope.show = scope.$eval(attributes.show);
+		link: function(scope, element, attributes) {
+			scope.title = attributes.header;
+			scope.className = attributes.class;
+			scope.close = scope.$eval(attributes.close);
+			scope.show = scope.$eval(attributes.show);
 
-				scope.$watch(attributes.show, function(value) {
-					scope.show = value;
-				});
-			}
+			scope.$watch(attributes.show, function(value) {
+				scope.show = value;
+			});
 		}
 	}
 });
