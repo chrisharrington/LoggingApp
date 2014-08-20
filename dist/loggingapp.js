d4750b0244aaaac802a5d4bf479863e6138c444f
Chris Harrington <chrisharrington99@gmail.com>
2014-08-20 09:43:34 -0600

    Finished sliding for slideshow.

--- dist/loggingapp.js
+++ dist/loggingapp.js
@@ -423,7 +423,9 @@ Logger.app.factory("newLog", function($rootScope, $timeout, once, feedback, coll
 			});
 		}
 	}
-});;Logger.app.directive("slideshow", function ($timeout, imageLoader) {
+});;Logger.app.directive("slideshow", function ($interval, imageLoader) {
+	var imageIndex = -1, urls = [];
+
 	return {
 		restrict: "E",
 		templateUrl: "templates/slideshow.html",
@@ -431,22 +433,10 @@ Logger.app.factory("newLog", function($rootScope, $timeout, once, feedback, coll
 			urls: "="
 		},
 		link: function(scope, element) {
-			$("h3").click(function() {
-				scope.$apply(function() {
-					slideImages(index++, element);
-					if (index > urls.length-1)
-						index = 0;
-				});
-			});
-
-			$(element).find("div.image-container.current").on(whichTransitionEvent(), function() {
+			element.find("div.image-container.first").on("transitionend oTransitionEnd webkitTransitionEnd", function() {
 				scope.$apply(function() {
 					scope.sliding = false;
 					scope.swap = !scope.swap;
-
-//							var current = $(element).find("div.image-container.current"), next = $(element).find("div.image-container.next");
-//							current.removeClass("current").addClass("next");
-//							next.removeClass("next").addClass("current");
 				});
 			});
 
@@ -458,35 +448,20 @@ Logger.app.factory("newLog", function($rootScope, $timeout, once, feedback, coll
 			var index = 0, urls = scope.urls;
 
 			imageLoader.load(urls).then(function() {
+				$interval(function() {
+					scope.sliding = true;
+					if (index > urls.length-1)
+						index = 0;
+				}, 5000);
+
 				scope.loading = false;
-
+				$(element).find("div.image-container.current").css("background-image", "url('" + urls[0] + "')");
+				if (urls.length > 1)
+					$(element).find("div.image-container.next").css("background-image", "url('" + urls[1] + "')");
 			});
-
-			function slideImages(index, element) {
-				scope.sliding = true;
-				$(element).find("div.image-container.current").css("background-image", "url('" + urls[index++] + "')");
-				$(element).find("div.image-container.next").css("background-image", "url('" + urls[index] + "')");
-			}
 		}
 	};
 });
-
-function whichTransitionEvent(){
-	var t;
-	var el = document.createElement('fakeelement');
-	var transitions = {
-		'transition':'transitionend',
-		'OTransition':'oTransitionEnd',
-		'MozTransition':'transitionend',
-		'WebkitTransition':'webkitTransitionEnd'
-	}
-
-	for(t in transitions){
-		if( el.style[t] !== undefined ){
-			return transitions[t];
-		}
-	}
-}
 ;Logger.app.directive("spinner", function() {
 	return {
 		restrict: "E",
