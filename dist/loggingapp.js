845ac30b58d5fac9a6911df6219ca25bdf336bb6
Chris Harrington <chrisharrington99@gmail.com>
2014-08-21 13:16:52 -0600

    Image loader class now removes appended image tags after loading.

--- dist/loggingapp.js
+++ dist/loggingapp.js
@@ -849,16 +849,25 @@ Logger.app.directive("feedback", function($rootScope) {
 });;Logger.app.service("imageLoader", function($q) {
 	return {
 		load: function(urls) {
-			var deferred = $q.defer();
-			var count = urls.length;
-			for (var i = 0; i < urls.length; i++)
-				$("body").append($("<img />", { style: "display: none !important", src: urls[i] }).on("load", function () {
+			var deferred = $q.defer(), count = urls.length, images = [];
+			for (var i = 0; i < urls.length; i++) {
+				var image = $("<img />", { style: "display: none !important", src: urls[i] });
+				images.push(image);
+				$("body").append(image.on("load", function () {
 					count--;
-					if (count == 0)
+					if (count == 0) {
+						_removeImages(images);
 						deferred.resolve();
+					}
 				}));
+			}
 			return deferred.promise;
 		}
+	};
+
+	function _removeImages(images) {
+		for (var i = 0; i < images.length; i++)
+			images[i].remove();
 	}
 });;Logger.app.factory("once", function() {
 	var keys = {};
